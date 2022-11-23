ef496faac3a6751d67714d9357df36c91192d361
Michael Nef <michael@nef.net.au>
2022-11-23 15:57:54 +0800

    updated changes

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,31 +1,50 @@
 import numpy as np
+from   scipy.optimize import root
+from   geographiclib.geodesic      import Geodesic    
+from mod_ocean_row import ocean_row 
+from dataclasses import dataclass
 
-class boat:
-    def __init__(self, vperf, m, Gw, GwA, Ga, start_lat, start_lon):
-        '''
-        vperf: scalar
-            velocity in perfect conditions [m/s]
-        m: scalar
-            mass
-        Gw: scalar
-            water friction constant
-        GwA: scalar
-            water friction constant sea anchor
-        Ga: scalar
-            air friction constant
-        start_lat: scalar
-            Starting position of the boat (latitude)
-        start_lon: scalar
-            Starting position of the boat (longitude)
-        '''
-        self.vperf = vperf
-        self.m = m
-        self.Gw = Gw
-        self.GwA = GwA
-        self.Ga = Ga
-        self.lat = start_lat
-        self.lon = start_lon
+@dataclass
+class State:
+    t: np.datetime64
+    lat: float
+    lon: float
+
+@dataclass
+class Action:    
+    is_rowing: bool
+    is_anchored: bool
+    phi: float
+
+    def __init__(self, is_rowing: bool, anchor = None, phi = None):
+        self.is_rowing = is_rowing
         
+        if (is_rowing):
+            if (phi == None):
+                raise ValueError("Anchor must be specified for rowing action")
+            else:
+                self.phi = phi
+
+            self.is_anchored = False
+        else:
+            if (anchor == None):
+                raise ValueError("Anchor must be specified for rowing action")
+            else:
+                self.is_anchored = anchor
+
+            self.phi = 0
+
+
+@dataclass
+class boat:
+    vperf: float 
+    m: float
+    Gw: float
+    GwA: float
+    Ga: float
+    start_lat: float
+    start_lon: float
+
     @property
     def rowing_force(self):
         '''
@@ -50,6 +69,9 @@ class ocean:
         self.boat = boat
         self.start_time = start_time
         self.target = target
+
+
+        self.TIMESTEP_SIZE = 10_000
         
     def get_velocities(self, time, positions):
         # datasets
@@ -64,7 +86,7 @@ class ocean:
         dsw_lon  = dsw.lon.values
         dsw_lat  = dsw.lat.values
         
-        Va_a, Vw_a = ocean_row.get_hindcast_at_loc(positions, time, dsa, dsw, dsa_time, dsa_lon, dsa_lat, dsw_time, dsw_lon, dsw_lat)
+        Va_a, Vw_a = ocean_row.get_hindcast_at_loc(np.reshape(positions, (1, 2)), time, dsa, dsw, dsa_time, dsa_lon, dsa_lat, dsw_time, dsw_lon, dsw_lat)
         
         # set hindcast velocities to zero when no data is available
         Va_a[np.isnan(Va_a)] = 0
@@ -72,21 +94,20 @@ class ocean:
         
         return Va_a, Vw_a
     
-    def friction_coeff(self, time, V):
-        '''
-        Calculates the friction coefficient Fwterm at a particular time
         
-        Parameters
-        ----------
-        V: vector [2]
-            velocity vector (e.g. of a boat)
-        '''
-        Va_a, Vw_a = get_velocities(time, [[self.boat.lat], [self.boat.lon]])
-        
-        
-        
+    def solve_v(self, state: State, angle, row, anc):
+        vw_all, va_all = self.get_velocities(state.t, [[state.lat], [state.lon]])
+
+        Vw = vw_all[0]
+        Va = va_all[0]
+
+        guess = self.boat.vperf + Va * 0.1
+
+        V = root(self.total_force, guess, args=(Vw, Va, self.get_ed(state.lat, state.lon, angle), row, anc), tol=.001).x
+        return V
     
-    def total_force(self, time, Frmag, ed, row, anc): 
+    
+    def total_force(self, V, *args): 
         '''
         Calculate sum of forces on rowing boat
 
@@ -94,11 +115,13 @@ class ocean:
         ----------
         V: vector [2]
             boat velocity
-        
-        ed:    Row direction [longitudinal,lateral] [-]
-        Frmag: rowing force [N]
-        row:   True when rowing [True/False]
-        anc:   Ture when anchor is dropped[True/False]
+
+        args:
+            vw: water vector [2]
+            va: air vector [2]
+            ed:    Row direction [longitudinal,lateral] [-]
+            row:   True when rowing [True/False]
+            anc:   Ture when anchor is dropped[True/False]
 
         Returns
         -------
@@ -106,18 +129,30 @@ class ocean:
             total force [longitudinal,lateral] [N]    
         '''    
 
+        # unpack args
+        Vw = args[0]
+        Va = args[1]
+        ed = args[2]
+        row = args[3]
+        anc = args[4]
+        
+        Frmag = self.boat.rowing_force
+
         # rowing force 
         Fr = int(row)*Frmag*ed
 
         # water friction sea anchor    
 
+
         Fwterm = np.sqrt((Vw-V).dot(Vw-V))*(Vw-V)
-        Fw     = Gw*Fwterm # water friction
-        Fwa    = int(anc)*GwA*Fwterm # sea anchor water friction, anchor is in when resting (row = 0)
+        Fw     = self.boat.Gw * Fwterm # water friction
+
+        Fwa    = int(anc) * self.boat.GwA * Fwterm # sea anchor water friction, anchor is in when resting (row = 0)
 
         # air friction 
-        Fa  = Ga*np.sqrt((Va-V).dot(Va-V))*(Va-V)
-        Fap = Fa.dot(ed)*ed # parallel component
+        Faterm = np.sqrt((Va-V).dot(Va-V))*(Va-V)
+        Fa  = self.boat.Ga * Faterm
+        Fap = Fa.dot(ed) * ed # parallel component
 
         # total force
         Ft  = Fw + Fwa + Fap + Fr
@@ -125,13 +160,45 @@ class ocean:
     
     def start(self):
         # Returns a representation of the starting state of the ocean
-        lat, long = self.startps
-        return (self.start_time, lat, long)
+        lat, lon = self.boat.start_lat, self.boat.start_lon
+        return (self.start_time, lat, lon)
     
-    def next_state(self, state, action):
+    def get_ed(self, lat, lon, phi):
+        # Returns the rowing direction
+        # row in direction of destination (bearing geodesic)
+        theta  = Geodesic.WGS84.Inverse(lat, lon, self.target[0], self.target[1])['azi1'] 
+        theta += phi # deviate from direction
+        ed     = np.array([np.sin(theta/180*np.pi),np.cos(theta/180*np.pi)])
+
+        return ed
+
+    def update_coordate_with_velocity(self, lat, lon, v, dt):
+        # Returns the updated coordinates
+        # update position
+        vmag = np.linalg.norm(v)
+       
+        azi1 = ocean_row.angle_between([0,1], v)
+        s12  = vmag * dt
+        
+        print("geodesic:",Geodesic.WGS84.Direct(lat, lon, azi1, s12))
+
+        Xnew = np.array(list(  map(Geodesic.WGS84.Direct(lat, lon, azi1, s12).get, ['lon2','lat2'])  )) # new location
+        
+        return Xnew[0], Xnew[1]
+    
+    def next_state(self, s: State, action: Action):
         # Takes the state, and the move to be applied.
         # Returns the new state.
-        pass
+
+        phi = action.phi
+
+        # Calculate the velocity applied to the boat
+        V = self.solve_v(s, phi, action.is_rowing, action.is_anchored)    
+
+        updated_lat, updated_lon = self.update_coordate_with_velocity(s.lat, s.lon, V, self.TIMESTEP_SIZE)
+
+        return State(s.t + self.TIMESTEP_SIZE, updated_lat, updated_lon)
+
 
     def legal_moves(self, state_history):
         # Takes a sequence of game states representing the full
@@ -139,7 +206,7 @@ class ocean:
         # are legal plays for the current player.
         
         # get_rowing_activity
-        
+        pass
         
     def winner(self, state_history):
         # Takes a sequence of game states representing the full
